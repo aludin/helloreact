48245644fb64912fb2045651b1df19716e8b6431
Anwar Ludin <anwar.ludin@algonents.com>
2021-05-26 23:59:30 +0200

    composant lien

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
+
+import Lien from "./composants/Lien"
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ouvrir Wikipédia
-        </a>
+        <Lien lien="https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal" texte="Wikipédia" />
       </header>
     </div>
   );
---
+++ src/composants/Lien.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "Lien.css";
+
+type ParametresLiens = {
+    lien: string;
+    texte: string;
+}
+
+const Lien = (props: ParametresLiens) => {
+    const { lien, texte } = props;
+    return (
+        <a
+            className="App-link"
+            href={lien}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            {texte}
+        </a>
+    )
+}
+
+export default Lien;
