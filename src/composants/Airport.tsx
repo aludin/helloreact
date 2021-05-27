67a03a5d7a5feb4f9125760c8345ad84fed34dac
Anwar Ludin <anwar.ludin@algonents.com>
2021-05-28 00:36:15 +0200

    useeffect implementation

--- src/composants/Airport.tsx
+++ src/composants/Airport.tsx
@@ -27,19 +27,30 @@ const Airport = () => {
         `https://applications.icao.int/dataservices/api/indicators-list?api_key=${apiKey}&state=${countryCode}&airports=${icaoCode}&format=json`
 
     useEffect(() => {
-        axiosClient.get<Array<AirportResult>>(url).then((response) => {
-            if (response.data && response.data.length > 0) {
-                setAirportName(response.data[0].airportName);
-                setCountryName(response.data[0].countryName);
-                setLatitude(response.data[0].latitude);
-                setLongitude(response.data[0].longitude);
-            }
-        }).catch((error) => {
-            console.log("error: " + JSON.stringify(error))
-        })
+        if (countryCode.length === 3 && icaoCode.length === 4) {
+            axiosClient.get<Array<AirportResult>>(url).then((response) => {
+                if (response.data && response.data.length > 0) {
+                    setAirportName(response.data[0].airportName);
+                    setCountryName(response.data[0].countryName);
+                    setLatitude(response.data[0].latitude);
+                    setLongitude(response.data[0].longitude);
+                }
+            }).catch((error) => {
+                console.log("error: " + JSON.stringify(error))
+            })
+        }
 
 
-    }, [])
+    }, [icaoCode, countryCode])
+
+
+    const onIcaoChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIcaoCode(event.target.value);
+    }
+
+    const onCountryCodeChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCountryCode(event.target.value);
+    }
 
 
 
@@ -47,8 +58,8 @@ const Airport = () => {
 
     return (
         <div style={{ display: "flex", height: "300px", flexDirection: "column", justifyContent: "space-between" }}>
-            <div style={{ display: "flex" }}><div style={{ marginRight: "10px" }}>Code icao</div><input value={icaoCode} /></div>
-            <div style={{ display: "flex" }}><div style={{ marginRight: "10px" }}>Code pays</div><input value={countryCode} /></div>
+            <div style={{ display: "flex" }}><div style={{ marginRight: "10px" }}>Code icao</div><input value={icaoCode} onChange={onIcaoChangeHandler} /></div>
+            <div style={{ display: "flex" }}><div style={{ marginRight: "10px" }}>Code pays</div><input value={countryCode} onChange={onCountryCodeChangeHandler} /></div>
 
             <div style={{ marginRight: "10px" }}>{`Country: ${countryName}`}</div>
             <div style={{ marginRight: "10px" }}>{`Airport: ${airportName}`}</div>
